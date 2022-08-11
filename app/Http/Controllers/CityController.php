8a62e388bf81d5e1c97d7880adc7016f43429a37
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-08-11 18:28:10 -0300

    feat: add CityController and related form requests

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCityRequest;
+use App\Http\Requests\UpdateCityRequest;
+use App\Models\City;
+
+class CityController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreCityRequest $request)
+    {
+        //
+    }
+
+    public function edit(City $city)
+    {
+        //
+    }
+
+    public function update(UpdateCityRequest $request, City $city)
+    {
+        //
+    }
+
+    public function destroy(City $city)
+    {
+        //
+    }
+}
