df42e83ba0974230a6825ae21c8b8c9472ab7c29
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-08-11 18:33:54 -0300

    feat: add StoreCityRequest rules

--- app/Http/Requests/StoreCityRequest.php
+++ app/Http/Requests/StoreCityRequest.php
@@ -14,7 +14,7 @@ class StoreCityRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'unique:cities']
         ];
     }
 }
