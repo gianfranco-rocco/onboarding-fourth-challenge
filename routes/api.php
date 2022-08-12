8585b88a8189c7f40238ec0b963caf48705539fa
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-08-12 14:22:53 -0300

    feat: add '/api/cities' route for storing cities

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::controller(CityController::class)
+    ->prefix('cities')
+    ->name('city.')
+    ->group(function () {
+        Route::post('/', 'store')->name('store');
+    }
+);
