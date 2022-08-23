0edfb6b5c23a2f4986993f95bdbe181354a372a4
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-08-23 14:45:29 -0300

    feat: clear cities from cache after eloquent event

--- app/Observers/CityObserver.php
+++ app/Observers/CityObserver.php
@@ -3,17 +3,18 @@
 namespace App\Observers;
 
 use App\Models\City;
+use Illuminate\Support\Facades\Cache;
 
 class CityObserver
 {
     public function created(City $city): void
     {
-        $city->clearCachedKeys();
+        $this->clearFromCache();
     }
 
     public function updated(City $city): void
     {
-        $city->clearCachedKeys();
+        $this->clearFromCache();
     }
 
     public function deleted(City $city): void
@@ -23,16 +24,21 @@ class CityObserver
 
         $city->airlines()->detach();
 
-        $city->clearCachedKeys();
+        $this->clearFromCache();
     }
 
     public function restored(City $city): void
     {
-        $city->clearCachedKeys();
+        $this->clearFromCache();
     }
 
     public function forceDeleted(City $city): void
     {
-        $city->clearCachedKeys();
+        $this->clearFromCache();
+    }
+
+    private function clearFromCache(): void
+    {
+        Cache::forget('cities');
     }
 }
